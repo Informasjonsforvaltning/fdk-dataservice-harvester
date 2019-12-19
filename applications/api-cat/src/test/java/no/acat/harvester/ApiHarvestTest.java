f584ac29eddf738c30a4627aede0522305fc16a9
Stig B. Dørmænen <stigbd@gmail.com>
2019-12-19 12:22:01 +0100

    [dsop] Add Brage Finans AS

--- applications/api-cat/src/test/java/no/acat/harvester/ApiHarvestTest.java
+++ applications/api-cat/src/test/java/no/acat/harvester/ApiHarvestTest.java
@@ -45,7 +45,7 @@ public class ApiHarvestTest {
 
         harvester.harvestAll();
 
-        verify(apiDocumentRepositoryMock, times(118)).createOrReplaceApiDocument(any());
+        verify(apiDocumentRepositoryMock, times(119)).createOrReplaceApiDocument(any());
     }
 
 
@@ -77,7 +77,7 @@ public class ApiHarvestTest {
 
         harvester.harvestAll();
 
-        verify(apiDocumentRepositoryMock, times(117)).createOrReplaceApiDocument(any());
+        verify(apiDocumentRepositoryMock, times(118)).createOrReplaceApiDocument(any());
     }
 
     @Test(expected = RuntimeException.class)
