f049b72e3baaf9b7637eafe7bdd1d06a0f0736b1
NilsOveTen <nils.ove.tendenes@acando.no>
2020-03-05 14:30:19 +0100

    chore: scrub elastic

--- src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataservice_harvester/controller/ApplicationStatusController.kt
+++ src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataservice_harvester/controller/ApplicationStatusController.kt
@@ -1,14 +1,12 @@
 package no.digdir.informasjonsforvaltning.fdk_dataservice_harvester.controller
 
-import no.digdir.informasjonsforvaltning.fdk_dataservice_harvester.repository.DataserviceRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ApplicationStatusController(val dataserviceRepository: DataserviceRepository) {
-
+class ApplicationStatusController() {
 
     @GetMapping("/ping")
     fun ping(): ResponseEntity<Void> {
@@ -18,18 +16,7 @@ class ApplicationStatusController(val dataserviceRepository: DataserviceReposito
     @GetMapping("/ready")
     fun ready(): ResponseEntity<Void> {
         try {
-            dataserviceRepository.count()
             return ResponseEntity.ok().build()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build()
-    }
-
-    @GetMapping("/count")
-    fun count(): ResponseEntity<Long> {
-        try {
-            return ResponseEntity.ok(dataserviceRepository.count())
         } catch (e: Exception) {
             e.printStackTrace()
         }
--- src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataservice_harvester/model/DatabaseModels.kt
+++ src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataservice_harvester/model/DatabaseModels.kt
@@ -1,22 +1,18 @@
 package no.digdir.informasjonsforvaltning.fdk_dataservice_harvester.model
 
 import no.digdir.informasjonsforvaltning.fdk_dataservice_harvester.generated.model.Dataservice
-import org.springframework.data.annotation.Id
-import org.springframework.data.elasticsearch.annotations.Document
 import java.net.URI
 import java.util.*
 
-@Document(indexName = "dataservices", type = "dataservice")
 data class DataserviceDB (
-    @Id val id: String,
+    val id: String,
     val dataservice: Dataservice,
     val metadataId: String,
     val catalogId: String
 )
 
-@Document(indexName = "dataservice_catalogs", type = "dataservice_catalog")
 data class CatalogDB (
-    @Id val id: String,
+    val id: String,
     val catalog: HarvestedCatalog,
     val metadataId: String
 )
@@ -29,9 +25,8 @@ data class HarvestedCatalog (
     val dataservices: List<String>
 )
 
-@Document(indexName = "dataservice_metadata", type = "metadata")
 data class HarvestMetadata (
-    @Id val id: String,
+    val id: String,
     val firstHarvested: Date,
     val lastHarvested: Date,
     val lastChanged: Date,
