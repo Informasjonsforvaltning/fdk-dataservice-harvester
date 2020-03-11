f52d6766a5e342abef1995b713acf53df6512e4b
NilsOveTen <nils.ove.tendenes@acando.no>
2020-03-11 13:39:21 +0100

    chore: self link from application properties

---
+++ src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataservice_harvester/configuration/ApplicationProperties.kt
@@ -0,0 +1,11 @@
+package no.digdir.informasjonsforvaltning.fdk_dataservice_harvester.configuration
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties("application")
+data class ApplicationProperties(
+    val dataserviceUri: String,
+    val catalogUri: String
+)
