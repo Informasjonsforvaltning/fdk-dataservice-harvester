0bdcb64e8c1a6b3122a1dd9f01b2c4d186803da3
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-08-06 12:27:59 +0200

    feat: #63 add SPARQL endpoints

--- src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataservice_harvester/fuseki/HarvestFuseki.kt
+++ src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataservice_harvester/fuseki/HarvestFuseki.kt
@@ -2,11 +2,14 @@ package no.digdir.informasjonsforvaltning.fdk_dataservice_harvester.fuseki
 
 import no.digdir.informasjonsforvaltning.fdk_dataservice_harvester.configuration.FusekiProperties
 import org.apache.jena.query.ReadWrite
+import org.apache.jena.query.ResultSet
+import org.apache.jena.query.ResultSetFormatter
 import org.apache.jena.rdf.model.Model
 import org.apache.jena.rdfconnection.RDFConnection
 import org.apache.jena.rdfconnection.RDFConnectionFuseki
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.io.ByteArrayOutputStream
 
 private val LOGGER = LoggerFactory.getLogger(HarvestFuseki::class.java)
 
@@ -36,6 +39,28 @@ class HarvestFuseki(private val fusekiProperties: FusekiProperties) {
             }
         }
 
+    fun queryAsk(query: String): Boolean =
+        dataServiceConnection().use {
+            it.begin(ReadWrite.READ)
+            try {
+                it.queryAsk(query)
+            } catch (ex: Exception) {
+                LOGGER.error("sparql-ask exception: $ex")
+                false
+            }
+        }
+
+    fun queryConstruct(query: String): Model? =
+        dataServiceConnection().use {
+            it.begin(ReadWrite.READ)
+            try {
+                it.queryConstruct(query)
+            } catch (ex: Exception) {
+                LOGGER.error("sparql-construct exception: $ex")
+                null
+            }
+        }
+
     fun queryDescribe(query: String): Model? =
         dataServiceConnection().use {
             it.begin(ReadWrite.READ)
@@ -47,10 +72,51 @@ class HarvestFuseki(private val fusekiProperties: FusekiProperties) {
             }
         }
 
+    fun querySelect(query: String, format: QueryResponseFormat): String? =
+        dataServiceConnection().use {
+            it.begin(ReadWrite.READ)
+            var result: String? = null
+            try {
+                it.queryResultSet(query) { qs ->
+                    result = when (format) {
+                        QueryResponseFormat.TEXT -> ResultSetFormatter.asText(qs).valuesOrNull(format)
+                        QueryResponseFormat.JSON -> querySelectWithJsonResponse(qs)?.valuesOrNull(format)
+                        QueryResponseFormat.XML -> ResultSetFormatter.asXMLString(qs).valuesOrNull(format)
+                    }
+                }
+            } catch (ex: Exception) {
+                LOGGER.error("sparql-select exception: $ex")
+                throw ex
+            }
+            return result
+        }
+
+    private fun querySelectWithJsonResponse(qs: ResultSet): String? {
+        val outputStream = ByteArrayOutputStream()
+        ResultSetFormatter.outputAsJSON(outputStream, qs)
+        return String(outputStream.toByteArray())
+    }
+
     fun saveWithGraphName(graphName: String, model: Model) =
         dataServiceConnection().use {
             it.begin(ReadWrite.WRITE)
             it.put(graphName, model)
         }
 
+}
+
+private fun String.valuesOrNull(format: QueryResponseFormat): String? =
+    when (format) {
+        QueryResponseFormat.TEXT -> if (this.split('\n').size > 5) this else null
+        QueryResponseFormat.JSON -> if (this.split("\"bindings\": [")[1]
+                .replace(Regex("[^A-Za-z0-9]"), "")
+                .isNotEmpty()) this else null
+        QueryResponseFormat.XML -> if (this.split("<results>")[1]
+                .split("</results>")[0]
+                .replace(Regex("[^A-Za-z0-9]"), "")
+                .isNotEmpty()) this else null
+    }
+
+enum class QueryResponseFormat {
+    TEXT, JSON, XML
 }
