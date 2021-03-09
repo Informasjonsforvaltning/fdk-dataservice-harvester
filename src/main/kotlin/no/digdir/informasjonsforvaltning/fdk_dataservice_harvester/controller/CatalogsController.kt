e97f6b82d209eea566594b24923f35351037d5b8
NilsOveTen <github.nils.ove@tendenes.com>
2021-03-09 08:05:21 +0100

    chore: rm generation of code from openapi-spec

--- src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataservice_harvester/controller/CatalogsController.kt
+++ src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataservice_harvester/controller/CatalogsController.kt
@@ -1,23 +1,32 @@
 package no.digdir.informasjonsforvaltning.fdk_dataservice_harvester.controller
 
-import no.digdir.informasjonsforvaltning.fdk_dataservice_harvester.generated.api.DcatApNoCatalogsApi
 import no.digdir.informasjonsforvaltning.fdk_dataservice_harvester.rdf.jenaTypeFromAcceptHeader
 import no.digdir.informasjonsforvaltning.fdk_dataservice_harvester.service.DataServiceService
 import org.apache.jena.riot.Lang
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import javax.servlet.http.HttpServletRequest
+import org.springframework.web.bind.annotation.*
 
 private val LOGGER = LoggerFactory.getLogger(CatalogsController::class.java)
 
 @Controller
-open class CatalogsController(private val dataServiceService: DataServiceService) : DcatApNoCatalogsApi {
+@CrossOrigin
+@RequestMapping(
+    value = ["/catalogs"],
+    produces = ["text/turtle", "text/n3", "application/rdf+json", "application/ld+json", "application/rdf+xml", "application/n-triples"]
+)
+open class CatalogsController(private val dataServiceService: DataServiceService) {
 
-    override fun getCatalogById(httpServletRequest: HttpServletRequest, id: String): ResponseEntity<String> {
+    @GetMapping("/{id}")
+    fun getCatalogById(
+        @RequestHeader(HttpHeaders.ACCEPT) accept: String?,
+        @PathVariable id: String
+    ): ResponseEntity<String> {
         LOGGER.info("get DataService catalog with id $id")
-        val returnType = jenaTypeFromAcceptHeader(httpServletRequest.getHeader("Accept"))
+        val returnType = jenaTypeFromAcceptHeader(accept)
 
         return if (returnType == Lang.RDFNULL) ResponseEntity(HttpStatus.NOT_ACCEPTABLE)
         else {
@@ -27,9 +36,10 @@ open class CatalogsController(private val dataServiceService: DataServiceService
         }
     }
 
-    override fun getCatalogs(httpServletRequest: HttpServletRequest): ResponseEntity<String> {
+    @GetMapping
+    fun getCatalogs(@RequestHeader(HttpHeaders.ACCEPT) accept: String?): ResponseEntity<String> {
         LOGGER.info("get all DataService catalogs")
-        val returnType = jenaTypeFromAcceptHeader(httpServletRequest.getHeader("Accept"))
+        val returnType = jenaTypeFromAcceptHeader(accept)
 
         return if (returnType == Lang.RDFNULL) ResponseEntity(HttpStatus.NOT_ACCEPTABLE)
         else ResponseEntity(dataServiceService.getAll(returnType ?: Lang.TURTLE), HttpStatus.OK)
--- src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataservice_harvester/controller/DataservicesController.kt
+++ src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataservice_harvester/controller/DataservicesController.kt
@@ -1,23 +1,32 @@
 package no.digdir.informasjonsforvaltning.fdk_dataservice_harvester.controller
 
-import no.digdir.informasjonsforvaltning.fdk_dataservice_harvester.generated.api.DcatApNoDataservicesApi
 import no.digdir.informasjonsforvaltning.fdk_dataservice_harvester.rdf.jenaTypeFromAcceptHeader
 import no.digdir.informasjonsforvaltning.fdk_dataservice_harvester.service.DataServiceService
 import org.apache.jena.riot.Lang
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import javax.servlet.http.HttpServletRequest
+import org.springframework.web.bind.annotation.*
 
 private val LOGGER = LoggerFactory.getLogger(DataServicesController::class.java)
 
 @Controller
-open class DataServicesController(private val dataServiceService: DataServiceService) : DcatApNoDataservicesApi {
+@CrossOrigin
+@RequestMapping(
+    value = ["/dataservices"],
+    produces = ["text/turtle", "text/n3", "application/rdf+json", "application/ld+json", "application/rdf+xml", "application/n-triples"]
+)
+open class DataServicesController(private val dataServiceService: DataServiceService) {
 
-    override fun getDataServiceById(httpServletRequest: HttpServletRequest, id: String): ResponseEntity<String> {
+    @GetMapping("/{id}")
+    fun getDataServiceById(
+        @RequestHeader(HttpHeaders.ACCEPT) accept: String?,
+        @PathVariable id: String
+    ): ResponseEntity<String> {
         LOGGER.info("get DataService with id $id")
-        val returnType = jenaTypeFromAcceptHeader(httpServletRequest.getHeader("Accept"))
+        val returnType = jenaTypeFromAcceptHeader(accept)
 
         return if (returnType == Lang.RDFNULL) ResponseEntity(HttpStatus.NOT_ACCEPTABLE)
         else {
